f3e2a3cc111d430f370c0771f6a7e6aa9699cca1
Ky Decker <itsevadecker@gmail.com>
2026-08-10 17:44:15 -0400

    Add Biome, Wrangler config, pre-commit hooks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://hypersensitive.eva.town",
+  site: "https://hypersensitive.ky.fyi",
   devToolbar: {
     enabled: false,
   },
